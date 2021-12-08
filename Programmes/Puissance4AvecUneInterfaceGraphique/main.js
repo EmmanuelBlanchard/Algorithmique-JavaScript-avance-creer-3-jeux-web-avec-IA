63fc71947045f663cd9d07c8c5b8f462e0ccab27
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-08 17:26:37 +0100

    AlgorithmiqueJavaScriptAvance:Creer3JeuxWebAvecIA-02-Puissance4AvecUneInterfaceGraphique-E4-GererLaFinDePartie

--- Programmes/Puissance4AvecUneInterfaceGraphique/main.js
+++ Programmes/Puissance4AvecUneInterfaceGraphique/main.js
@@ -1,26 +1,34 @@
+const tour = document.querySelector("#tour");
+const alert = document.querySelector(".alert");
+var joueurEnCours = 1;
+var finJeu = false;
+
 jeu.initialisation();
 jeu.afficherPuissance4();
 
-var joueurEnCours = 1;
-
-function jouer(colonne){
-    var ligneVide = jeu.retournerLigneCaseVideColonne(colonne);
-    jeu.jouerCase(joueurEnCours,ligneVide,colonne);
-    jeu.afficherPuissance4();
-
-    if(joueurEnCours === 1){
-        joueurEnCours = 2;
-    } else {
-        joueurEnCours = 1;
+function jouer(colonne) {
+    if(!finJeu) {
+        var ligneVide = jeu.retournerLigneCaseVideColonne(colonne);
+        if(ligneVide !== -1) {
+            jeu.jouerCase(joueurEnCours,ligneVide,colonne);
+            jeu.afficherPuissance4();
+            if(jeu.verificationFinJeu(joueurEnCours)) {
+                gererFinJeu();
+            }
+        
+            if(joueurEnCours === 1) {
+                joueurEnCours = 2;
+                tour.innerHTML = "Tour du joueur 2";
+            } else {
+                joueurEnCours = 1;
+                tour.innerHTML = "Tour du joueur 1";
+            }
+        }
     }
-    // var ligneVide = -1;
-    // var colonne = -1;
-    // while(ligneVide === -1 || colonne <= 0 || colonne > 7) {
-    //     console.log("Choisir une colonne à un emplacement vide");
-    //     var colonne = jeu.saisirColonne();
-    //     var ligneVide = jeu.retournerCaseVideColonne(colonne);
-    // }
-    // jeu.jouerCase(joueur,ligneVide,colonne);
-    // jeu.afficherPuissance4();
-    // return jeu.verificationFinJeu(joueur);
+}
+
+function gererFinJeu() {
+    finJeu = true;
+    alert.innerHTML = "Fin du jeu";
+    alert.classList.remove("d-none");
 }
