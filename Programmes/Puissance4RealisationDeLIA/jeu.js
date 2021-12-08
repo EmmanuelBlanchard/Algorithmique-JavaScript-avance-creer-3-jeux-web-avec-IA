79e713db320f867846fe8656563fd44f8933fd95
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-08 18:47:10 +0100

    AlgorithmiqueJavaScriptAvance:Creer3JeuxWebAvecIA-03-Puissance4RealisationDeLIA-E1-ActiverLIA

---
+++ Programmes/Puissance4RealisationDeLIA/jeu.js
@@ -0,0 +1,157 @@
+var jeu = {
+    puissance4 : [],
+    nombreColonnes : 7,
+    nombreLignes : 6,
+    joueur1Caractere : "x",
+    joueur2Caractere : "o",
+
+    /**
+     * 
+     */
+    initialisation : function() {
+        this.puissance4 = toolbox.initialiserTableauVide(this.nombreLignes,this.nombreColonnes,0);
+    },
+
+    /**
+     * Permet d'afficher un tableau de puissance 4 
+     */
+    afficherPuissance4 : function() {
+        const jeu = document.querySelector("#jeu");
+        jeu.innerHTML = "";
+        
+        var content = "<table>";
+            for(var i = 0; i < this.nombreLignes; i++){
+                content += "<tr>";
+                for(var j = 0 ; j < this.nombreColonnes; j++){
+                    content += "<td class='border text-center' style='width:100px;height:100px'>";
+                    if(this.puissance4[i][j] === 0){
+                        content += "";
+                    } else if(this.puissance4[i][j] === 1){
+                        content += "<img src='./images/J1.png' class='bg-danger rounded-circle' />";
+                    } if(this.puissance4[i][j] === 2){
+                        content += "<img src='./images/J2.png' class='bg-info rounded-circle' />";
+                    }
+                    content += "</td>";
+                }
+                content += "</tr>";
+            }
+            content += "<tr>";
+                content += '<td><button type="button" class="btn btn-secondary" onClick="jouer(1)">Colonne 1</button></td>';
+                content += '<td><button type="button" class="btn btn-secondary" onClick="jouer(2)">Colonne 2</button></td>';
+                content += '<td><button type="button" class="btn btn-secondary" onClick="jouer(3)">Colonne 3</button></td>';
+                content += '<td><button type="button" class="btn btn-secondary" onClick="jouer(4)">Colonne 4</button></td>';
+                content += '<td><button type="button" class="btn btn-secondary" onClick="jouer(5)">Colonne 5</button></td>';
+                content += '<td><button type="button" class="btn btn-secondary" onClick="jouer(6)">Colonne 6</button></td>';
+                content += '<td><button type="button" class="btn btn-secondary" onClick="jouer(7)">Colonne 7</button></td>';
+            content += "</tr>";
+        content += "</table>";
+        jeu.innerHTML = content;
+    },
+
+    jouerCase : function(joueur,ligne,colonne) {
+        this.puissance4[ligne][colonne-1] = joueur;
+    },
+
+    /**
+     * Fonction permettant de retourner la premiere ligne d'une colonne
+     * @param {Number} colonne Retourne -1 si la colonne est pleine
+     */
+     retournerLigneCaseVideColonne : function(colonne) {
+        // Si la colonne est pleine on retourne -1
+        for(var i = this.nombreLignes-1; i >= 0; i--) {
+            if(this.verificationCaseVide(i,colonne)) return i;
+        }
+        return -1;
+    },
+
+    /**
+     * Fonction permettant de retourner si une cellule est vide ou pas (retourne true / false)
+     * @param {Number} ligne 
+     * @param {Number} colonne 
+     */
+    verificationCaseVide : function(ligne,colonne) {
+        return this.puissance4[ligne][colonne-1] === 0;
+    },
+
+    /**
+     * Fonction permettant de saisir une colonne
+     */
+    saisirColonne : function() {
+        return parseInt(toolbox.saisieString("Quelle colonne ? "));
+    },
+
+    /**
+     * Fonction permettant de verifier si un joueur a gagné
+     * @param {Number} joueur 
+     */
+    verificationFinJeu : function(joueur) {
+        if(this.verificationLigneFinJeu(joueur) || this.verificationColonneFinJeu(joueur) || this.verificationDiagonaleFinJeu(joueur)) {
+            return true;
+        }
+        return false;
+    },
+
+    /**
+     * Fonction permettant de verifier si un joueur a gagné sur une ligne
+     * @param {Number} joueur 
+     */
+    verificationLigneFinJeu : function(joueur) {
+        for(var i = this.nombreLignes-1; i >= 0; i--) {
+            for(var j = 0; j < this.nombreColonnes-3; j++) {
+                if( this.puissance4[i][j] === joueur &&
+                    this.puissance4[i][j+1] === joueur &&
+                    this.puissance4[i][j+2] === joueur &&
+                    this.puissance4[i][j+3] === joueur
+                    ) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    },
+
+    /**
+     * Fonction permettant de verifier si un joueur a gagné sur une colonne
+     * @param {Number} joueur 
+     */
+    verificationColonneFinJeu : function(joueur) {
+        for(var i = 0; i < this.nombreColonnes; i++) {
+            for(var j = this.nombreLignes-4; j >= 0; j--) {
+                if( this.puissance4[j][i] === joueur &&
+                    this.puissance4[j+1][i] === joueur &&
+                    this.puissance4[j+2][i] === joueur &&
+                    this.puissance4[j+3][i] === joueur
+                    ) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    },
+
+    /**
+     * Fonction permettant de verifier si un joueur a gagné sur une diagonale
+     * @param {Number} joueur 
+     */
+    verificationDiagonaleFinJeu : function(joueur) {
+        for(var i = this.nombreLignes-1; i >= 3; i--) {
+            for(var j = 0; j < this.nombreColonnes; j++) {
+                if( this.puissance4[i][j] === joueur &&
+                    this.puissance4[i-1][j+1] === joueur &&
+                    this.puissance4[i-2][j+2] === joueur &&
+                    this.puissance4[i-3][j+3] === joueur
+                    ) {
+                        return true;
+                }
+                if( this.puissance4[i][j] === joueur &&
+                    this.puissance4[i-1][j-1] === joueur &&
+                    this.puissance4[i-2][j-2] === joueur &&
+                    this.puissance4[i-3][j-3] === joueur
+                    ) {
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+}
---
+++ Programmes/Puissance4RealisationDeLIA/main.js
@@ -0,0 +1,74 @@
+const tour = document.querySelector("#tour");
+const alert = document.querySelector(".alert");
+const messageJoueur1 = document.querySelector("#joueur1");
+const messageJoueur2 = document.querySelector("#joueur2");
+var joueurEnCours = 1;
+var finJeu = false;
+
+var pointJoueur1 = 0;
+var pointJoueur2 = 0;
+
+var isIAON = false;
+
+initialisationTableau();
+
+function startIA() {
+    isIAON = !isIAON;
+}
+
+function jouer(colonne) {
+    jouerCase(colonne);
+    if(isIAON) {
+        colonneIA = 1;
+        jouerCase(colonneIA);
+    }
+}
+
+function jouerCase(colonne) {
+    if(!finJeu) {
+        var ligneVide = jeu.retournerLigneCaseVideColonne(colonne);
+        if(ligneVide !== -1) {
+            jeu.jouerCase(joueurEnCours,ligneVide,colonne);
+            jeu.afficherPuissance4();
+            if(jeu.verificationFinJeu(joueurEnCours)) {
+                gererFinJeu();
+            }
+        
+            if(joueurEnCours === 1) {
+                joueurEnCours = 2;
+                tour.innerHTML = "Tour du joueur 2";
+            } else {
+                joueurEnCours = 1;
+                tour.innerHTML = "Tour du joueur 1";
+            }
+        }
+    }
+}
+
+function initialisationTableau() {
+    finJeu = false;
+    joueurEnCours = 1;
+    alert.classList.add("d-none");
+    var contentJoueur1 = "<img src='./images/J1.png' class='bg-danger rounded-circle' /><br />";
+    contentJoueur1 += pointJoueur1; 
+    messageJoueur1.innerHTML = contentJoueur1;
+    var contentJoueur2 = "<img src='./images/J2.png' class='bg-info rounded-circle' /><br />";
+    contentJoueur2 += pointJoueur2; 
+    messageJoueur2.innerHTML = contentJoueur2;
+
+    jeu.initialisation();
+    jeu.afficherPuissance4();
+}
+
+function gererFinJeu() {
+    finJeu = true;
+    var contentAlert = "Partie terminée, le gagnant est : Joueur " + joueurEnCours + " <br />";
+    contentAlert += '<button type="button" class="btn btn-secondary" onClick = initialisationTableau()>Recommencer</button>';
+    alert.innerHTML = contentAlert;
+    alert.classList.remove("d-none");
+    if(joueurEnCours === 1) {
+        pointJoueur1++;
+    } else {
+        pointJoueur2++;
+    }
+}
