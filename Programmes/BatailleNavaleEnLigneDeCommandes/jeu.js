a5abadec4a0a3f0db406453dbeb425f2af15af0b
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-09 18:16:43 +0100

    AlgorithmiqueJavaScriptAvance:Creer3JeuxWebAvecIA-04-BatailleNavaleEnLigneDeCommandes-E2-AffichageDeLaGrille

--- Programmes/BatailleNavaleEnLigneDeCommandes/jeu.js
+++ Programmes/BatailleNavaleEnLigneDeCommandes/jeu.js
@@ -1,16 +1,49 @@
+var toolbox = require("./toolbox.js");
+
 var jeu = {
     nombreColonnes : 5,
     nombreLignes : 5,
     grille : [],
 
+    nombreCaseJoueur1 : 0,
+    nombreCaseJoueur2 : 0,
+
     initialisation : function() {
+        this.grille = toolbox.initialiserTableauVide(this.nombreLignes,this.nombreColonnes,0);
+        this.positionnerBateau(3,1);
+        this.nombreCaseJoueur1 += 3;
+        this.positionnerBateau(2,1);
+        this.nombreCaseJoueur1 += 2;
+        this.positionnerBateau(3,2);
+        this.nombreCaseJoueur2 += 3;
+        this.positionnerBateau(2,2);
+        this.nombreCaseJoueur2 += 2;
+    },
+
+    positionnerBateau : function(taille,joueur) {
 
     },
+
     afficherGrille : function() {
-
+        for(var i = 0 ; i < this.nombreLignes ; i++) {
+            var texte = "";
+            for(var j = 0 ; j < this.nombreColonnes ; j++){
+                texte += "| ";
+                if(this.grille[i][j] === 0 ) {
+                    texte += "_";
+                } else if(this.grille[i][j] === 1 ) {
+                    texte += "x";
+                } else if(this.grille[i][j] === 2 ) {
+                    texte += "o";
+                }
+                texte += " |";
+            }
+            console.log(texte);
+        }
     },
+
     jouerCase : function(ligne,colonne) {
 
-    }
+    },
 }
 module.exports = jeu;
