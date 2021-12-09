59e416154ee86b7f5a6fc8891f65630d1ae8e7ec
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-09 17:58:47 +0100

    AlgorithmiqueJavaScriptAvance:Creer3JeuxWebAvecIA-04-BatailleNavaleEnLigneDeCommandes-E1-InitialisationDeLaStructure

---
+++ Programmes/BatailleNavaleEnLigneDeCommandes/jeu.js
@@ -0,0 +1,16 @@
+var jeu = {
+    nombreColonnes : 5,
+    nombreLignes : 5,
+    grille : [],
+
+    initialisation : function() {
+
+    },
+    afficherGrille : function() {
+
+    },
+    jouerCase : function(ligne,colonne) {
+
+    }
+}
+module.exports = jeu;
---
+++ Programmes/BatailleNavaleEnLigneDeCommandes/main.js
@@ -0,0 +1,17 @@
+var jeu = require("./jeu.js");
+var toolbox = require("./toolbox.js");
+
+jeu.initialisation();
+jeu.afficherGrille();
+while(true) {
+    if(jouer()) return; // Joueur1
+    if(jouer()) return; // Joueur2
+}
+
+function jouer() {
+    var saisieLigne = toolbox.saisirEntier("Jouer en quelle ligne ? : ");
+    var saisieColonne = toolbox.saisirEntier("Jouer en quelle colonne ? : ");
+    var isOver = jeu.jouerCase(saisieLigne,saisieColonne);
+    jeu.afficherGrille();
+    return isOver;
+}
