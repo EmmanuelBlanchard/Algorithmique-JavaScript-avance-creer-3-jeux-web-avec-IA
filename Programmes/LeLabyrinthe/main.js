d505a8e1f5896f20821f5fd5c5f207477663f471
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-10 16:49:02 +0100

    AlgorithmiqueJavaScriptAvance:Creer3JeuxWebAvecIA-06-LeLabyrinthe-E2-PositionnerLOursEtLePanda

--- Programmes/LeLabyrinthe/main.js
+++ Programmes/LeLabyrinthe/main.js
@@ -1,6 +1,8 @@
 var nombreLignes = 4;
 var nombreColonnes = 4;
 
+var positionPlayer = [0,0];
+
 const monJeu = document.querySelector("#jeu");
 
 var ligne1 = [5,10,1,7];
@@ -14,11 +16,21 @@ afficherLabyrinthe(tableauJeu);
 function afficherLabyrinthe(tableauJeu) {
     monJeu.innerHTML = "";
     var content ="<table>";
-    for (var i = 0 ; i < tableauJeu.length;i++){
+    for (var i = 0 ; i < tableauJeu.length;i++) {
         content += "<tr>";
-        for (var j = 0 ; j < tableauJeu[i].length;j++){
+        for (var j = 0 ; j < tableauJeu[i].length;j++) {
             content += "<td>";
                 content += "<img src='images/"+tableauJeu[i][j]+".png' />";
+                if(i === nombreLignes -1 && j === nombreColonnes -1) {
+                    var pandaLigne = 25 + 100 * i;
+                    var pandaColonne = 25 + 100 * j;
+                    content += "<img src='images/panda.png' style='width:50px;height:50px;position:absolute;left:"+pandaColonne+"px;top:"+pandaLigne+"px'/>";
+                }
+                if(i === positionPlayer[0] && j === positionPlayer[1]) {
+                    var oursLigne = 25 + 100 * positionPlayer[0];
+                    var oursColonne = 25 + 100 * positionPlayer[1];
+                    content += "<img src='images/bear.png' style='width:50px;height:50px;position:absolute;left:"+oursColonne+"px;top:"+oursLigne+"px'/>";
+                }
             content += "</td>";
         }
         content += "</tr>";
