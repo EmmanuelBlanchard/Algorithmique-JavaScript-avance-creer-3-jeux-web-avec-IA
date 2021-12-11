4eda9e203bae708feb1d6036457cd6b38cc58fff
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-11 16:18:56 +0100

    AlgorithmiqueJavaScriptAvance:Creer3JeuxWebAvecIA-08-UnOutilPourCreerFacilementNosLabyrinthes-E2-ClicsSurLesImages

--- Programmes/UnOutilPourCreerFacilementNosLabyrinthes/main.js
+++ Programmes/UnOutilPourCreerFacilementNosLabyrinthes/main.js
@@ -15,10 +15,37 @@ function afficherGrille() {
         content += "<tr>";
         for (var j = 0 ; j < tableauJeu[i].length ; j++) {
             content += "<td>";
-                content+= "<img src='images/"+tableauJeu[i][j]+ ".png' id='" + i + "-" + j + "' />";
+                content+= "<img src='images/"+tableauJeu[i][j]+ ".png' id='" + i + "-" + j + "' oncontextmenu='return false'/>";
             content += "</td>";
         }
         content += "</tr>";
     }
     monJeu.innerHTML = content;
 }
+
+function changeImage(ligne,colonne,add) {
+    if(add){
+        if(tableauJeu[ligne][colonne] === 15) tableauJeu[ligne][colonne] = 0;
+        tableauJeu[ligne][colonne]++;
+    } else {
+        if(tableauJeu[ligne][colonne] === 0) tableauJeu[ligne][colonne] = 15;
+        tableauJeu[ligne][colonne]--;
+    }
+    afficherGrille();
+}
+
+monJeu.addEventListener("click", function() {
+    var id = event.target.id;
+    var idTiret = id.indexOf("-");
+    var ligne = id.substring(0,idTiret);
+    var colonne = id.substring((idTiret+1), id.length);
+    changeImage(ligne,colonne,true);
+});
+
+monJeu.addEventListener("contextmenu", function() {
+    var id = event.target.id;
+    var idTiret = id.indexOf("-");
+    var ligne = id.substring(0,idTiret);
+    var colonne = id.substring((idTiret+1), id.length);
+    changeImage(ligne,colonne,false);
+});
